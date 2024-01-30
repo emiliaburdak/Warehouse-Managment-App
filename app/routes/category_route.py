7f1022bda606ab2f79537774c699b7ab668ce23d
Em <emiliaburdak@gmail.com>
2024-01-30 17:11:57 +0100

    Refactor router

--- app/routes/category_route.py
+++ app/routes/category_route.py
@@ -1,22 +1,77 @@
 from fastapi import HTTPException, APIRouter, Request
 from fastapi.encoders import jsonable_encoder
 from app.models.category import Category, UpdateCategory
-from app.models.part import Part
 
 router = APIRouter()
+
+
+def ensure_that_parent_category_exist(db, parent_name):
+    parent_category = db.categories.find_one({'name': parent_name})
+    if parent_name != '' and parent_category is None:
+        raise HTTPException(status_code=400, detail="Category with this parent name does not exist")
+
+
+def ensure_that_category_does_not_exist(db, category_name):
+    existing_category_with_same_name = db.categories.find_one({'name': category_name})
+    if existing_category_with_same_name:
+        raise HTTPException(status_code=409, detail="Category with this name already exists")
+
+
+def ensure_category_not_base_if_parts_exist(db, category):
+    parts_belongs_category = db.parts.find_one({'category': category})
+    if parts_belongs_category:
+        raise HTTPException(status_code=400,
+                            detail="This category cannot be a base category because it has associated parts.")
+
+
+def update_child_categories_parent_name(db, category, fields_to_update):
+    child_categories = db.categories.find({'parent_name': category})
+    for child_category in child_categories:
+        db.categories.update_one({'name': child_category['name']},
+                                 {'$set': {'parent_name': fields_to_update['name']}})
+
+
+def ensure_that_category_exist(db, category):
+    existing_category = db.categories.find_one({'name': category})
+    if not existing_category:
+        raise HTTPException(status_code=404, detail="Category not found")
+
+
+def update_child_category_parent_name(db, category_to_delete_parent_name, child_categories):
+    for child_category in child_categories:
+        db.categories.update_one({'name': child_category['name']},
+                                 {'$set': {'parent_name': category_to_delete_parent_name}})
+
+
+def extract_parent_name(db, category):
+    category_to_delete = db.categories.find_one({'name': category})
+    category_to_delete_parent_name = category_to_delete['parent_name']
+    return category_to_delete_parent_name
+
+
+def prevent_category_delete_if_part_associated_child_category(db, child_categories):
+    for child_category in child_categories:
+        if db.parts.find_one({'category': child_category['name']}):
+            raise HTTPException(status_code=400,
+                                detail="You can not delete this category because, "
+                                       "there are parts that belong to child category " + child_category['name'])
+
+
+def prevent_category_delete_if_part_associated(db, category):
+    parts_belongs_category = db.parts.find_one({'category': category})
+    if parts_belongs_category:
+        raise HTTPException(status_code=400,
+                            detail="You can not delete this category because, "
+                                   "there are parts that belong to this category.")
 
 
 @router.post("/categories/", status_code=201)
 def add_category(request: Request, category_dto: Category):
     db = request.app.database
 
-    existing_category_with_same_name = db.categories.find_one({'name': category_dto.name})
-    if existing_category_with_same_name:
-        raise HTTPException(status_code=409, detail="Category with this name number already exists")
+    ensure_that_category_does_not_exist(db, category_dto.name)
 
-    parent_category = db.categories.find_one({'name': category_dto.parent_name})
-    if category_dto.parent_name != '' and parent_category is None:
-        raise HTTPException(status_code=400, detail="Category with this parent name does not exist")
+    ensure_that_parent_category_exist(db, category_dto.parent_name)
 
     db.categories.insert_one(category_dto.model_dump())
     return category_dto
@@ -26,31 +81,25 @@ def add_category(request: Request, category_dto: Category):
 def update_category(category: str, request: Request, update_category_dto: UpdateCategory):
     db = request.app.database
 
-    existing_category = db.categories.find_one({'name': category})
-    if not existing_category:
-        raise HTTPException(status_code=404, detail="Category not found")
+    ensure_that_category_exist(db, category)
 
     fields_to_update = {field: value for field, value in update_category_dto.model_dump(exclude_unset=True).items() if
                         value is not None}
 
-    # if there is category to update, change all parent categories in child_categories
+    # if there is category to update, update all parent_name in child_categories and update category
     if 'name' in fields_to_update:
-        child_categories = db.categories.find({'parent_name': category})
-        for child_category in child_categories:
-            db.categories.update_one({'name': child_category['name']},
-                                     {'$set': {'parent_name': fields_to_update['name']}})
+        update_child_categories_parent_name(db, category, fields_to_update)
         db.categories.update_one({'name': category}, {'$set': {'name': fields_to_update['name']}})
 
     if 'parent_name' in fields_to_update:
+        # if category is not base category
         if fields_to_update['parent_name'] != '':
             db.categories.update_one({'name': category}, {'$set': {'parent_name': fields_to_update['parent_name']}})
         else:
             # check if there are parts with this category because if true then this category can not be base category
-            parts_belongs_category = db.parts.find_one({'category': category})
-            if parts_belongs_category:
-                raise HTTPException(status_code=400,
-                                    detail="This category cannot be a base category because it has associated parts.")
+            ensure_category_not_base_if_parts_exist(db, category)
             db.categories.update_one({'name': category}, {'$set': {'parent_name': ''}})
+
     updated_category = db.categories.find_one({'name': fields_to_update['name']})
     return jsonable_encoder(updated_category, exclude=['_id'])
 
@@ -76,37 +125,22 @@ def get_all_categories(request: Request):
 def delete_category(category: str, request: Request):
     db = request.app.database
     # check if category has parts if true -> error
-    parts_belongs_category = db.parts.find_one({'category': category})
-    if parts_belongs_category:
-        raise HTTPException(status_code=400,
-                            detail="You can not delete this category because, "
-                                   "there are parts that belong to this category.")
+    prevent_category_delete_if_part_associated(db, category)
 
     # check if child category has parts if true -> error
     child_categories = list(db.categories.find({'parent_name': category}))
-    for child_category in child_categories:
-        if db.parts.find_one({'category': child_category['name']}):
-            raise HTTPException(status_code=400,
-                                detail="You can not delete this category because, "
-                                       "there are parts that belong to child category " + child_category['name'])
+    prevent_category_delete_if_part_associated_child_category(db, child_categories)
 
     # extract parent_name to use it while assigning child_category
-    category_to_delete = db.categories.find_one({'name': category})
-    category_to_delete_parent_name = category_to_delete['parent_name']
+    category_to_delete_parent_name = extract_parent_name(db, category)
 
     # delete category
     delete_result = db.categories.delete_one({'name': category})
     if delete_result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Part not found")
 
-    # assign child_category['parent_name'] to new parent_name (if it was base category assign to "" so it will be new base category)
+    # assign child_category['parent_name'] to new parent_name
     if category_to_delete_parent_name != "":
-        for child_category in child_categories:
-            db.categories.update_one({'name': child_category['name']},
-                                     {'$set': {'parent_name': category_to_delete_parent_name}})
+        update_child_category_parent_name(db, category_to_delete_parent_name, child_categories)
     else:
-        for child_category in child_categories:
-            db.categories.update_one({'name': child_category['name']},
-                                     {'$set': {'parent_name': ""}})
-
-
+        update_child_category_parent_name(db, "", child_categories)
--- app/routes/part_route.py
+++ app/routes/part_route.py
@@ -6,41 +6,50 @@ from app.models.part import Part, UpdatePart
 router = APIRouter()
 
 
+def ensure_that_part_does_not_exist(db, serial_number):
+    existing_part = db.parts.find_one({'serial_number': serial_number})
+    if existing_part:
+        raise HTTPException(status_code=409, detail="Part with this serial number already exists")
+
+
+def handle_no_parent_category(db, category):
+    category = db.categories.find_one({'name': category})
+    if category is None or category['parent_name'] == '':
+        raise HTTPException(status_code=400, detail="Incorrect category, part can't be created")
+
+
+def ensure_that_part_exist(db, serial_number):
+    existing_part = db.parts.find_one({'serial_number': serial_number})
+    if not existing_part:
+        raise HTTPException(status_code=404, detail="Part not found")
+
+
 @router.post("/parts/", status_code=201)
 def add_part(request: Request, part_dto: Part):
     db = request.app.database
 
-    existing_part = db.parts.find_one({'serial_number': part_dto.serial_number})
-    if existing_part:
-        raise HTTPException(status_code=409, detail="Part with this serial number already exists")
+    ensure_that_part_does_not_exist(db, part_dto.serial_number)
 
-    handle_no_parent_category(db, part_dto)
+    handle_no_parent_category(db, part_dto.category)
 
     db.parts.insert_one(part_dto.model_dump())
     return part_dto
-
-
-def handle_no_parent_category(db, part_dto):
-    category = db.categories.find_one({'name': part_dto.category})
-    if category is None or category['parent_name'] == '':
-        raise HTTPException(status_code=400, detail="Incorrect category, part can't be created")
 
 
 @router.put("/parts/{serial_number}")
 def update_part(serial_number: str, request: Request, update_part_dto: UpdatePart):
     db = request.app.database
 
-    existing_part = db.parts.find_one({'serial_number': serial_number})
-    if not existing_part:
-        raise HTTPException(status_code=404, detail="Part not found")
+    ensure_that_part_exist(db, serial_number)
 
     fields_to_update = {field: value for field, value in update_part_dto.model_dump(exclude_unset=True).items() if
                         value is not None}
 
     if 'category' in fields_to_update:
-        handle_no_parent_category(db, update_part_dto)
+        handle_no_parent_category(db, update_part_dto.category)
 
     db.parts.update_one({'serial_number': serial_number}, {'$set': fields_to_update})
+
     updated_part = db.parts.find_one({'serial_number': serial_number})
     return jsonable_encoder(updated_part, exclude=['_id'])
 
@@ -88,8 +97,3 @@ def search_part(request: Request, searched_parameters: UpdatePart = Depends()):
 
     searched_parts = db.parts.find(query)
     return [Part(**part) for part in searched_parts]
-
-
-
-
-
